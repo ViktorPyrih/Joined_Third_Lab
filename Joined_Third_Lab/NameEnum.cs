c8d4aa723bf300d27549046d3a0426dc0099a5e6
vikto <viktor.pi2017@gmail.com>
2021-03-23 20:35:18 +0200

    Added enums

---
+++ Joined_Third_Lab/NameEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Joined_Third_Lab
+{
+    public enum NameEnum
+    {
+        VIKTOR = 1,
+        MYKYTA,
+        MAKSYM
+    }
+}
---
+++ Joined_Third_Lab/TaskEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Joined_Third_Lab
+{
+    public enum TaskEnum
+    {
+        TASK_1 = 1,
+        TASK_2
+    }
+}
